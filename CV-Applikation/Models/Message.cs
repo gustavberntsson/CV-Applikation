1b170f2a79ee1d6b9ca918e20e1eea7642a883e6
Julia042 <julia.segerlind@hotmail.com>
2024-12-30 12:43:27 +0100

    error messages

--- CV-Applikation/Models/Message.cs
+++ CV-Applikation/Models/Message.cs
@@ -7,10 +7,21 @@ namespace CV_Applikation.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "AvsändarID är obligatoriskt.")]
 		public string SenderId { get; set; }
+
+        [Required(ErrorMessage = "MottagarID är obligatoriskt.")]
         public string ReceiverId { get; set; }
+
+        [Required(ErrorMessage = "Meddelandeinnehåll är obligatoriskt.")]
+        [StringLength(1000, ErrorMessage = "Meddelandeinnehållet får inte överskrida 1000 tecken.")]
         public string Content { get; set; }
+
+        [Required]
         public Boolean IsRead { get; set; }
+
+        [Required]
 		public System.DateTime Date { get; set; }
 
         [ForeignKey(nameof(SenderId))]
