2f23ac82f2b2f9d8a23794ffd414e19d4cdf1829
Julia042 <julia.segerlind@hotmail.com>
2025-01-02 15:45:48 +0100

    header

---
+++ CV-Applikation/Migrations/20250102131254_css.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CV_Applikation.Migrations
+{
+    /// <inheritdoc />
+    public partial class css : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "ContactInformation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "ContactInformation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
