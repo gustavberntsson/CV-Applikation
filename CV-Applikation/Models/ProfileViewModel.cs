ac942d1c021eca706f56acde42d4b0fda9205a37
Oskar Andersson <154226472+fuwzie@users.noreply.github.com>
2025-01-03 13:06:30 +0100

    Fixat så icke användare kommer till rätt meddelanderuta + fixade äcklig profil som inte funkade och sök

--- CV-Applikation/Models/ProfileViewModel.cs
+++ CV-Applikation/Models/ProfileViewModel.cs
@@ -3,6 +3,7 @@
 public class ProfileViewModel
 {
     public string ProfileName { get; set; }
+    public string ProfileId { get; set; }
     public string? ImageUrl { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
