a48e2f9b2810058b618c8f9094084fa792b94fb2
G B <gustavberntsson@protonmail.com>
2025-01-13 10:25:13 +0100

    HomeController > Try/Catch

--- CV-Applikation/Controllers/HomeController.cs
+++ CV-Applikation/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace CV_Applikation.Controllers
         private readonly ILogger<HomeController> _logger;
         private UserContext context;
         private UserManager<User> userManager;
+
         public HomeController(ILogger<HomeController> logger, UserContext service, UserManager<User> userManagerr)
         {
             _logger = logger;
@@ -20,69 +21,98 @@ namespace CV_Applikation.Controllers
 
         public async Task<IActionResult> Index()
         {
-
-            //Den aktuella användaren hämtas, kopplat till inloggningen.
-            var currentUser = await userManager.GetUserAsync(User);
-
-            //Urval av CVs hämtas som uppfyller särskilda kriterier.
-            var Cvs = await context.CVs
-                .Include(cv => cv.User)
-                .Where(cv => !cv.IsPrivate) //Privata CV:n filtreras bort 
-                .Where(cv => cv.User.IsPrivate == false) //CV:n som har en privat ägare filtreras bort.
-                .Where(cv => cv.User.IsEnabled == true) //CV:n som har en inaktiverad ägare filtreras bort.
-                .Include(cv => cv.Educations)
-                .Include(cv => cv.Languages)
-                .Include(cv => cv.Skills)
-                .ToListAsync();
-
-            //Senaste projektet hämtas som följer särskilda kriterier.
-            var lastProject = await context.Projects
-                .Where(p => p.Owner.IsPrivate == false)
-                .Where(p => p.Owner.IsEnabled == true)
-                .OrderByDescending(p => p.CreatedAt) //Projekten sorteras efter senaste skapande.
-                .FirstOrDefaultAsync(); //Det senaste (första) projektet hämtas. 
-
-
-            bool isUserInProject = false;
-
-            if (currentUser != null && lastProject != null)
+            try
             {
-                //Kontrollerar ifall det existerar en koppling mellan den aktuella användaren och det senaste projektet.
-                isUserInProject = await context.ProjectUsers
-                    .AnyAsync(pu => pu.ProjectId == lastProject.ProjectId && pu.UserId == currentUser.Id);
+                //Den aktuella användaren hämtas, kopplat till inloggningen.
+                var currentUser = await userManager.GetUserAsync(User);
+
+                //Urval av CVs hämtas som uppfyller särskilda kriterier.
+                var Cvs = await context.CVs
+                    .Include(cv => cv.User)
+                    .Where(cv => !cv.IsPrivate) //Privata CV:n filtreras bort 
+                    .Where(cv => cv.User.IsPrivate == false) //CV:n som har en privat ägare filtreras bort.
+                    .Where(cv => cv.User.IsEnabled == true) //CV:n som har en inaktiverad ägare filtreras bort.
+                    .Include(cv => cv.Educations)
+                    .Include(cv => cv.Languages)
+                    .Include(cv => cv.Skills)
+                    .ToListAsync();
+
+                //Senaste projektet hämtas som följer särskilda kriterier.
+                var lastProject = await context.Projects
+                    .Where(p => p.Owner.IsPrivate == false)
+                    .Where(p => p.Owner.IsEnabled == true)
+                    .OrderByDescending(p => p.CreatedAt) //Projekten sorteras efter senaste skapande.
+                    .FirstOrDefaultAsync(); //Det senaste (första) projektet hämtas. 
+
+                bool isUserInProject = false;
+
+                if (currentUser != null && lastProject != null)
+                {
+                    //Kontrollerar ifall det existerar en koppling mellan den aktuella användaren och det senaste projektet.
+                    isUserInProject = await context.ProjectUsers
+                        .AnyAsync(pu => pu.ProjectId == lastProject.ProjectId && pu.UserId == currentUser.Id);
+                }
+
+                var model = new HomeViewModel
+                {
+                    CVs = Cvs,
+                    ProjectLatest = lastProject,
+                    IsUserInProject = isUserInProject
+                };
+
+                return View(model);
             }
-
-
-            var model = new HomeViewModel
+            catch (Exception ex)
             {
-                CVs = Cvs,
-                ProjectLatest = lastProject,
-                IsUserInProject = isUserInProject
-            };
-
-            return View(model);
+                _logger.LogError($"Ett fel uppstod: {ex.Message}");
+                return View("Error");
+            }
         }
 
         public IActionResult Profile()
         {
-            var currentUser = userManager.GetUserAsync(User).Result;
-            if (currentUser == null)
-                return RedirectToAction("Login", "Account");
-            return RedirectToAction("Profile", "Account", new { UserId = currentUser.Id });
+            try
+            {
+                var currentUser = userManager.GetUserAsync(User).Result;
+                if (currentUser == null)
+                    return RedirectToAction("Login", "Account");
+                return RedirectToAction("Profile", "Account", new { UserId = currentUser.Id });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ett fel uppstod vid hämtning av profil: {ex.Message}");
+                return RedirectToAction("Error");
+            }
         }
 
         public IActionResult Message()
         {
-            var currentUser = userManager.GetUserAsync(User).Result;
-            if (currentUser == null)
-                return RedirectToAction("Login", "Account");
-            return RedirectToAction("Message", "Message");
+            try
+            {
+                var currentUser = userManager.GetUserAsync(User).Result;
+                if (currentUser == null)
+                    return RedirectToAction("Login", "Account");
+                return RedirectToAction("Message", "Message");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ett fel uppstod vid hämtning av meddelanden: {ex.Message}");
+                return RedirectToAction("Error");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ett fel uppstod: {ex.Message}");
+                return View(new ErrorViewModel { RequestId = "Okänt" });
+            }
         }
     }
 }
